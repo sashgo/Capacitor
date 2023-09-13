65164cb1a69f3bd0ab84f57c755ac2ce7975c3b2
Oleksandr Bobel <oleksandr.bobel@3shape.com>
2023-09-13 15:09:10 +0300

    Init

---
+++ android/app/src/main/java/ionic/unitecloud/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.unitecloud;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.unitecloud',
+  appName: 'unitecloud',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
